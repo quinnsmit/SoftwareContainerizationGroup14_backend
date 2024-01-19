dbf6c86a6dfda7436023d798cec9eeab38846314
Aditya Srivastava <49762864+Aditya746@users.noreply.github.com>
2024-01-19 23:39:56 +0100

    Update app.py

--- API/app.py
+++ API/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import pymysql
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://@localhost/SC_Group14'
